6fc9b114a9589643ddb0ed04cc6a081d1c22f275
Sophiya Qureshi <sophiya2001qureshi@gmail.com>
2023-11-04 23:23:03 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState,useEffect} from 'react';
+
+
 
 function App() {
+
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Replace 'your_api_url_here' with the actual API URL you want to fetch from.
+    fetch('http://api.nobelprize.org/v1/prize.json')
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data);
+        setLoading(false);
+        console.log(data);
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+        setLoading(false);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>API Data</h1>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <div>
+          <button onClick={() => console.log(data)}>Log Data to Console</button>
+          <ul>
+            {data[1].map((item, index) => (
+              <li key={index}>{item.name}</li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
